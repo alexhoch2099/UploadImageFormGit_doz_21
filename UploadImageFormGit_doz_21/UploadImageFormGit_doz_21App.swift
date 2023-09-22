83dcf285ae0a5cb00f13122d84bcc87b5eaaf40e
Alexander Hoch <zancorgithub@zancor.de>
2023-09-22 08:56:44 +0200

    Initial Commit

---
+++ UploadImageFormGit_doz_21/UploadImageFormGit_doz_21App.swift
@@ -0,0 +1,17 @@
+//
+//  UploadImageFormGit_doz_21App.swift
+//  UploadImageFormGit_doz_21
+//
+//  Created by Student on 22.09.23.
+//
+
+import SwiftUI
+
+@main
+struct UploadImageFormGit_doz_21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
